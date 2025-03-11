cf580be71d7ad8ff4e82f643a50ac28c74092952
Mauricio Braga <braga.mauricio@gmail.com>
2025-03-11 09:33:14 -0300

    Adicionada a tela inicial do jogo.

--- Assets/scripts/TelaInicialGameState.cs
+++ Assets/scripts/TelaInicialGameState.cs
@@ -3,8 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 public class TelaInicialGameState:GameBaseState
 {
+    private GameObject telaInicialJogo;
      public override void enterState(GameStateManager gameState) {
         Debug.Log("Entramos na tela inicial");
+        telaInicialJogo = GameObject.Find("tela_inicial_Snake_1280_1060_0");
+        telaInicialJogo.GetComponent<SpriteRenderer>().enabled = true;
      }
     public override void updateState(GameStateManager gameState) {
             if (Input.GetKeyDown(KeyCode.Space))  {
@@ -14,6 +17,7 @@ public class TelaInicialGameState:GameBaseState
 
     public override void leaveState(GameStateManager gameState)  {
         Debug.Log("Saindo da tela inicial");
+        telaInicialJogo.GetComponent<SpriteRenderer>().enabled = false;
 
     }
 }
