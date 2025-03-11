fb11d812ac5b31709a3a95f47c4b200686540de2
Mauricio Braga <braga.mauricio@gmail.com>
2025-03-11 09:52:33 -0300

    Adicionada a tela gráfica de créditos.

--- Assets/scripts/TelaCreditosGameState.cs
+++ Assets/scripts/TelaCreditosGameState.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class TelaCreditosGameState:GameBaseState
 {
+     private GameObject telaCreditosJogo;
      public override void  enterState(GameStateManager gameState) {
         Debug.Log("Entramos na tela créditos");
+        telaCreditosJogo = GameObject.Find("tela_creditos_Snake_1280_1060_0");
+        telaCreditosJogo.GetComponent<SpriteRenderer>().enabled = true;
      }
     public override void updateState(GameStateManager gameState) {
             if (Input.GetKeyDown(KeyCode.Space))  {
@@ -13,6 +16,7 @@ public class TelaCreditosGameState:GameBaseState
 
     public override void leaveState(GameStateManager gameState)  {
         Debug.Log("Saindo da tela créditos");
+        telaCreditosJogo.GetComponent<SpriteRenderer>().enabled = false;
 
     }
 }
